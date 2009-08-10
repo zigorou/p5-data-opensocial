9548d704a5fb2084101a3c95f79c3d04ba61ebc0
U-ZIGONOTE\zigorou <zigorou@zigonote.(none)>
2009-08-10 10:57:15 +0900

    init

---
+++ Data-OpenSocial/lib/Data/OpenSocial.pm
@@ -0,0 +1,33 @@
+package Data::OpenSocial;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Data::OpenSocial -
+
+=head1 SYNOPSIS
+
+  use Data::OpenSocial;
+
+=head1 DESCRIPTION
+
+Data::OpenSocial is
+
+=head1 AUTHOR
+
+Toru Yamaguchi E<lt>zigorou@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ Data-OpenSocial/lib/Data/OpenSocial/PluralPersonField.pm
@@ -0,0 +1,105 @@
+package Data::OpenSocial::PluralPersonField;
+
+use Any::Moose;
+
+use URI;
+use XML::LibXML;
+
+has 'value' => (
+    is => 'rw',
+    isa => 'Str',
+    required => 0,
+    predicate => 'has_value',
+);
+
+has 'type' => (
+    is => 'rw',
+    isa => 'Str',
+    required => 0,
+    predicate => 'has_type',
+);
+
+has 'primary' => (
+    is => 'rw',
+    isa => 'Bool',
+    required => 0,
+    predicate => 'has_primary',
+);
+
+has 'ns_uri' => (
+    is => 'ro',
+    isa => 'URI',
+    default => sub {
+        URI->new('http://ns.opensocial.org/2008/opensocial');
+    },
+    lazy => 1,
+);
+
+has 'ns_prefix' => (
+    is => 'rw',
+    isa => 'Str',
+    default => '',
+);
+
+has 'element' => (
+    is => 'ro',
+    isa => 'XML::LibXML::Element',
+    default => sub { 
+        my $self = shift;
+        my $elem = XML::LibXML::Element->new($self->element_name);
+        $elem->setNamespace(
+            $self->ns_uri,
+            $self->ns_prefix,
+        );
+
+        $elem;
+    },
+    lazy => 1,
+);
+
+has 'element_name' => (
+    is => 'rw',
+    isa => 'Str',
+    required => 1,
+);
+
+has 'child_elements' => (
+    is => 'ro',
+    isa => 'ArrayRef[Str]',
+    default => sub { [qw/value type primary/] },
+);
+
+sub as_xml {
+    my $self = shift;
+    my $element = $self->element;
+
+    no strict 'refs';
+
+    for my $child_element_name (@{$self->child_elements}) {
+        my $code = $self->can("has_" . $child_element_name);
+        next unless ($code && $code->($self));
+        my $child_element = XML::LibXML::Element->new($child_element_name);
+        $child_element->appendText($self->$child_element_name);
+        $element->appendChild($child_element);
+    }
+
+    return $element;
+}
+
+sub as_object {
+    my $self = shift;
+
+    my $object = +{};
+
+    for my $child_element_name (@{$self->child_elements}) {
+        my $code = $self->can("has_" . $child_element_name);
+        next unless ($code && $code->($self));
+        $object->{$child_element_name} = $self->$child_element_name;
+    }
+
+    return $object;
+}
+
+no Any::Moose;
+
+__PACKAGE__->meta->make_immutable;
